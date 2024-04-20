dfeb0f95dde7c814e07dbb8fa5445d149d1886bb
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-04-20 13:42:17 +0530

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,6 @@ export default function Home() {
                          <nav>
                          </nav>
                          <div className="flex space-between">
-                              {/* Action buttons */}
                               <a href="https://dev.to/retr0kernel/securing-smart-contracts-our-ml-powered-solidity-vulnerability-scanner-webapp-2m1j-temp-slug-9404343?preview=b2bc9f2a4906972b2af042c5ea957c30006e23a386733c5b3c06ff6d576e6dd3e01f066c43a93b30aa816b68173d892517e04d1c53ab774355a3ca51" className="p-2 px-7">Blog</a>
                               {/* <a href="#" className="p-2 bg-blue-600 rounded-md">Login</a> */}
                               <a href="#" className="bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 shadow-lg">Login</a>
