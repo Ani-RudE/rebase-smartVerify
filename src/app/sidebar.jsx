dc02df6ade38a5f4916eab41151c28c85ccbcd25
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-04-21 12:10:46 +0530

    sidebar

--- src/app/sidebar.jsx
+++ src/app/sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Sidebar = () => {
      return (
-          <div className="bg-gray-800 text-white w-64 space-y-6 py-16 px-3 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
+          <div className="z-10 bg-gray-800/[0.6] text-white w-64 space-y-6 py-16 px-3 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
                <nav>
                     <a href="/profile" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-purple-700 hover:text-white">
                          Profile
